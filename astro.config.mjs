52badab4b51f7a94ad3ddfbcd41f162215a4e31d
jcharmer <jasoncharmer@gmail.com>
2026-05-18 20:54:32 -0700

    Configure custom domain: on.crewdriverapp.com
    Add CNAME file and update Astro site URL.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://jcharmer.github.io',
+  site: 'https://on.crewdriverapp.com',
   trailingSlash: 'ignore',
 });
